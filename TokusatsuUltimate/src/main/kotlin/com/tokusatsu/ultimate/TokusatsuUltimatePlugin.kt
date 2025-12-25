1984b371f485242b7846af9da66efab1e335cd3a
Joshua Pogi Bascode <joeycabopomaam@gmail.com>
2025-12-25 13:49:57 +0800

    Add HiAnime and TokusatsuUltimate extensions with settings functionality

--- TokusatsuUltimate/src/main/kotlin/com/tokusatsu/ultimate/TokusatsuUltimatePlugin.kt
+++ TokusatsuUltimate/src/main/kotlin/com/tokusatsu/ultimate/TokusatsuUltimatePlugin.kt
@@ -1,8 +1,16 @@
 package com.tokusatsu.ultimate
 
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.AcraApplication.Companion.getKey
+import com.lagradost.cloudstream3.AcraApplication.Companion.setKey
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
+
+enum class TokusatsuServerList(val link: Pair<String, Boolean>) {
+    TOKU555_COM("https://toku555.com" to true),
+    TOKUZILLA_NET("https://tokuzilla.net" to true),
+}
 
 @CloudstreamPlugin
 class TokusatsuUltimatePlugin: Plugin() {
@@ -10,5 +18,18 @@ class TokusatsuUltimatePlugin: Plugin() {
         // Registers the main API for this plugin
         registerMainAPI(TokusatsuUltimate())
         registerExtractorAPI(TokusatsuUltimate.P2pplay())
+        this.openSettings = openSettings@{ ctx ->
+            val activity = ctx as AppCompatActivity
+            val frag = TokusatsuBottomFragment(this)
+            frag.show(activity.supportFragmentManager, "")
+        }
+    }
+
+    companion object {
+        var currentTokusatsuServer: String
+            get() = getKey("TOKUSATSU_CURRENT_SERVER") ?: TokusatsuServerList.TOKU555_COM.link.first
+            set(value) {
+                setKey("TOKUSATSU_CURRENT_SERVER", value)
+            }
     }
 }
